80a1c183d7ea11d9924444c9a33826d71a5cfd6a
Damian Bachórz <damian.bachorz@briisk.co>
2016-05-08 12:36:17 +0200

    first component

---
+++ app.ts
@@ -0,0 +1,22 @@
+/// <reference path="node_modules/angular2/ts/typings/node/node.d.ts"/>
+/// <reference path="node_modules/angular2/typings/browser.d.ts"/>
+import { bootstrap } from "angular2/platform/browser";
+import { Component } from "angular2/core";
+
+@Component({
+  selector: 'hello-world',
+  template: `
+  <div>
+    Hello {{ name }}
+  </div>
+  `
+})
+class HelloWorld {
+  name: string;
+  
+  constructor(){
+    this.name = 'Damian';
+  }
+}
+
+bootstrap(HelloWorld);
