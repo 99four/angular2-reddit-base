de4428733ba21637a1e2d2886de3e008bb30ec2f
Damian Bachórz <damian.bachorz@briisk.co>
2016-05-11 21:25:48 +0200

    removed console log

--- app.ts
+++ app.ts
@@ -122,14 +122,11 @@ class RedditApp {
   
   constructor(){
     this.articles = [
-      new Article('Angular 2', 'http://angular.io', 3),
-      new Article('Fullstack', 'http://fullstack.io', 2),
-      new Article('Angular Homepage', 'http://angular.io', 1),
+      new Article('Angular 2', 'http://angular.io', 3)
     ];
   }
   
   addArticle(title: HTMLInputElement, link: HTMLInputElement): void {
-    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
     this.articles.push(new Article(title.value, link.value, 0));
     title.value = '';
     link.value = '';
